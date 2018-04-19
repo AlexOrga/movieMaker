1449ed5efd31908dd95627263c8d510af264b3d4
Alex Orga <alexorga.dev@gmail.com>
2018-04-19 17:42:44 -0500

    finished basic styling for the options list

--- javascripts/categoriesDom.js
+++ javascripts/categoriesDom.js
@@ -1,16 +1,23 @@
-const categoriesOutputDiv = document.getElementById('checkboxes');
+const categoriesOutputDiv = document.getElementById('left-column');
 // const data = require('./data');
 
 const printDomString = (movieElem, categories) => {
   let domString = '';
-  domString +=  `<div>`;
+  domString +=  `<div class="options">`;
   categories.forEach((category) => {
+    domString +=  `<div class="category-container">`;
     domString +=  `<h3>${category.categoryName}</h3>`;
+    domString +=    `<div class="checkboxes row">`;
     movieElem.forEach((movieElement) => {
       if (movieElement.categoryId === category.id) {
-        domString += `<p>${movieElement.name}</p>`;
+        domString += `<div class="option-item col-xs-3">`;
+        domString += `<input type="checkbox" name="checkbox">`;
+        domString += `<p class="option-name">${movieElement.name}</p>`;
+        domString += `</div>`;
       }
     });
+    domString +=    `</div>`;
+    domString +=  `</div>`;
   });
   domString +=  `</div>`;
   return domString;
