7fa2d4cfabc00789b116373aefc0a4365e8a4c17
Alex Orga <alexorga.dev@gmail.com>
2018-04-19 21:32:52 -0500

    added progress bar functionality and styling

--- javascripts/progressBar.js
+++ javascripts/progressBar.js
@@ -4,10 +4,10 @@ const printToDom = (divId, string) => {
   document.getElementById(divId).innerHTML = string;
 };
 
-const buildProgressBar = (currentSpent, budgetMax) => {
+const buildProgressBar = (currentSpent, budgetMax, percent) => {
   let domString = '';
   domString += `<div class="progress">`;
-  domString +=    `<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="${currentSpent}" aria-valuemin="0" aria-valuemax="${budgetMax}" style="width: 50%">`;
+  domString +=    `<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="${currentSpent}" aria-valuemin="0" aria-valuemax="${budgetMax}" style="width: ${percent}%">`;
   domString +=    `</div>`;
   domString +=  `</div>`;
   printToDom('progress-bar', domString);
