65bdcdfdc1a9598d805a3ee70e00c44c1692e067
Alex Orga <alexorga.dev@gmail.com>
2018-05-26 13:52:41 -0500

    added readme and category checker

--- javascripts/costWindow.js
+++ javascripts/costWindow.js
@@ -2,13 +2,17 @@ const data = require('./data');
 const canMakeOrNot = require('./canMakeMovie');
 
 const makeReceipt = (elements) => {
+  const totalCostValue = data.getTotalCostValue();
   let domString = '';
   elements.forEach((element) => {
     domString += `<div class="receipt-item">`;
     domString +=    `<p>${element.name}: $${element.cost}</p>`;
     domString +=  `</div>`;
   });
-  canMakeOrNot();
+  domString += `<div class="receipt-item">`;
+  domString +=    `<h4><u><em>Current Cost: $${totalCostValue}</em></u></h4>`;
+  domString +=  `</div>`;
+  canMakeOrNot(elements);
   printReceipt('costs', domString);
 };
 
